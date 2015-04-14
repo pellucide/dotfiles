bc6dc75094f116b4bc191f9009199f5df718e436
Daniel Suo <danielsuo@gmail.com>
2015-04-14 15:31:11 -0400

    add chrome and slack repos

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,4 +1,10 @@
-sudo add-apt-repository ppa:webupd8team/sublime-text-3
+# Sublime repo
+sudo add-apt-repository ppa:webupd8team/sublime-text-3\
+
+# Chrome repo
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+
 sudo apt-get update
 
 apps=(
@@ -14,3 +20,8 @@ apps=(
 sudo pip install mackup
 cp mackup/.mackup.cfg ~/.mackup.cfg
 mackup restore
+
+# Install slack
+sudo apt-add-repository -y ppa:rael-gc/scudcloud
+sudo apt-get update
+sudo apt-get install scudcloud
