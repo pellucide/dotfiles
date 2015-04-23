25364fdb9d4316d3c99af1e0bb02f2b706a973c6
Daniel Suo <danielsuo@gmail.com>
2015-04-23 10:28:39 -0400

    add spotify

--- mackup/.emacs.d/init.el
+++ mackup/.emacs.d/init.el
@@ -221,3 +221,4 @@
 (require 'google-c-style)
 (add-hook 'c-mode-common-hook 'google-set-c-style)
 (add-hook 'c-mode-common-hook 'google-make-newline-indent)
+(put 'upcase-region 'disabled nil)
--- ubuntu.sh
+++ ubuntu.sh
@@ -39,12 +39,19 @@ git config --global credential.helper cache
 # Autokey
 xmodmap -e "keysym Caps_Lock = Hyper_L" -e "keycode 66 = BackSpace" -e "remove Mod4 = Hyper_L" -e "add Mod3 = Hyper_L" -e "keycode 66 = Hyper_L"
 
-# By hand
-# - Add autokey to startup applications (automate by moving autokey.desktop to ~/.config/autostart)
-
 # Docker
 # wget -qO- https://get.docker.com/ | sh
 # sudo usermod -aG docker floored
+
+# Spotify
+sudo apt-add-repository -y "deb http://repository.spotify.com stable non-free" &&
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59 &&
+sudo apt-get update -qq &&
+sudo apt-get install spotify-client
+
+# By hand
+# - Add autokey to startup applications (automate by moving autokey.desktop to ~/.config/autostart)
+
 
 # To do
 # - Fix hyper key
