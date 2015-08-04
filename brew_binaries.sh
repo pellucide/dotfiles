8ad22ae086cc731c0cea0a5467162e1b71bb78fe
Daniel Suo <danielsuo@gmail.com>
2015-08-04 13:54:43 -0400

    Add git global

--- brew_binaries.sh
+++ brew_binaries.sh
@@ -1,18 +1,19 @@
 binaries=(
 	graphicsmagick	# Image manipulation library
-	rename			    # CLI: rename files
-	trash			      # CLI: send files to trash 
-	tree			      # CLI: view directory as tree 
-	ack				      # CLI: better grep
-	hub				      # GitHub helpers for git
-	git				      # Version control
-  dockutil        # Manage OS X dock
-  autoconf        # Autotools
-  automake        # Autotools
-  node            # Node.js
-  duti            # Set default applications
-  bchunk          # disk format stuff
-  jq              # JSON parser for bash
+	rename		# CLI: rename files
+	trash		# CLI: send files to trash 
+	tree            # CLI: view directory as tree 
+	ack             # CLI: better grep
+	hub             # GitHub helpers for git
+	git		# Version control
+        dockutil        # Manage OS X dock
+        autoconf        # Autotools
+        automake        # Autotools
+        node            # Node.js
+        duti            # Set default applications
+        bchunk          # disk format stuff
+        jq              # JSON parser for bash
+        global          # GNU Global
 )
 
 echo "Installing brew binaries..."
--- mackup/.emacs.d/init.el
+++ mackup/.emacs.d/init.el
@@ -108,19 +108,25 @@
 ;; A lovely manifest
 (defconst package-list
   '(
+
+    
 ;     anzu                                ; displays current/total matches on isearch
 ;     company                             ; autocomplete framework
 ;     duplicate-thing                     ; M-c to duplicate
-;     drag-stuff                          ; Line transposition
+
+    ;; Text manipulation
+    drag-stuff                          ; Line transposition
 
     ;; Themes
     monokai-theme                       ; Preferred theme
     nyan-mode                           ; Nyan-Mode
+
+    ;; Navigation
+    helm                                ; Finding-things framework
+    helm-projectile                     ; Helm Projectile support
+    helm-gtags                          ; Helm gtags support
+    helm-swoop                          ; Helm swoop support
     
-;     helm                                ; Finding-things framework
-;     helm-projectile                     ; Helm Projectile support
-;     helm-gtags                          ; Helm gtags support
-;     helm-swoop                          ; Helm swoop support
 ;     function-args                       ; C: show inline arguments for function
 ;     clean-aindent-mode                  ; Cleanup double-RET whitespace
 ;     comment-dwim-2                      ; Comment-dwim with cycline
@@ -237,11 +243,11 @@
 ; (global-set-key (kbd "s-{") 'windmove-left)
 ; (global-set-key (kbd "s-}") 'windmove-right)
 
-; ;; Package: drag-stuff
-; (require 'drag-stuff)
-; (drag-stuff-global-mode)
-; (global-set-key (kbd "M-n") 'drag-stuff-down)
-; (global-set-key (kbd "M-p") 'drag-stuff-up)
+;; Package: drag-stuff
+(require 'drag-stuff)
+(drag-stuff-global-mode)
+(global-set-key (kbd "M-n") 'drag-stuff-down)
+(global-set-key (kbd "M-p") 'drag-stuff-up)
 
 ; (add-to-list 'load-path "~/.emacs.d")
 
@@ -249,3 +255,158 @@
 ; (add-hook 'c-mode-common-hook 'google-set-c-style)
 ; (add-hook 'c-mode-common-hook 'google-make-newline-indent)
 ; (put 'upcase-region 'disabled nil)
+
+;; Package: helm
+(require 'helm-config)
+(require 'helm-grep)
+
+;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebihnd tab to do persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+(define-key helm-grep-mode-map (kbd "<return>")  'helm-grep-mode-jump-other-window)
+(define-key helm-grep-mode-map (kbd "n")  'helm-grep-mode-jump-other-window-forward)
+(define-key helm-grep-mode-map (kbd "p")  'helm-grep-mode-jump-other-window-backward)
+
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
+(setq
+ helm-scroll-amount 4 ; scroll 4 lines other window using M-<next>/M-<prior>
+ helm-ff-search-library-in-sexp t ; search for library in `require' and `declare-function' sexp.
+ helm-split-window-in-side-p t ;; open helm buffer inside current window, not occupy whole other window
+ helm-candidate-number-limit 500 ; limit the number of displayed canidates
+ helm-ff-file-name-history-use-recentf t
+ helm-move-to-line-cycle-in-source t ; move to end or beginning of source when reaching top or bottom of source.
+ helm-buffers-fuzzy-matching t          ; fuzzy matching buffer names when non-nil
+                                        ; useful in helm-mini that lists buffers
+
+ )
+
+(add-to-list 'helm-sources-using-default-as-input 'helm-source-man-pages)
+
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-h SPC") 'helm-all-mark-rings)
+(global-set-key (kbd "C-c h o") 'helm-occur)
+
+(global-set-key (kbd "C-c h C-c w") 'helm-wikipedia-suggest)
+
+(global-set-key (kbd "C-c h x") 'helm-register)
+;; (global-set-key (kbd "C-x r j") 'jump-to-register)
+
+(define-key 'help-command (kbd "C-f") 'helm-apropos)
+(define-key 'help-command (kbd "r") 'helm-info-emacs)
+(define-key 'help-command (kbd "C-l") 'helm-locate-library)
+
+;; use helm to list eshell history
+(add-hook 'eshell-mode-hook
+          #'(lambda ()
+              (define-key eshell-mode-map (kbd "M-l")  'helm-eshell-history)))
+
+;;; Save current position to mark ring
+(add-hook 'helm-goto-line-before-hook 'helm-save-current-pos-to-mark-ring)
+
+;; show minibuffer history with Helm
+(define-key minibuffer-local-map (kbd "M-p") 'helm-minibuffer-history)
+(define-key minibuffer-local-map (kbd "M-n") 'helm-minibuffer-history)
+
+(define-key global-map [remap find-tag] 'helm-etags-select)
+
+(define-key global-map [remap list-buffers] 'helm-buffers-list)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PACKAGE: helm-swoop                ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Locate the helm-swoop folder to your path
+(require 'helm-swoop)
+
+;; Change the keybinds to whatever you like :)
+(global-set-key (kbd "C-c h o") 'helm-swoop)
+(global-set-key (kbd "C-c s") 'helm-multi-swoop-all)
+
+;; When doing isearch, hand the word over to helm-swoop
+(define-key isearch-mode-map (kbd "M-i") 'helm-swoop-from-isearch)
+
+;; From helm-swoop to helm-multi-swoop-all
+(define-key helm-swoop-map (kbd "M-i") 'helm-multi-swoop-all-from-helm-swoop)
+
+;; Save buffer when helm-multi-swoop-edit complete
+(setq helm-multi-swoop-edit-save t)
+
+;; If this value is t, split window inside the current window
+(setq helm-swoop-split-with-multiple-windows t)
+
+;; Split direcion. 'split-window-vertically or 'split-window-horizontally
+(setq helm-swoop-split-direction 'split-window-vertically)
+
+;; If nil, you can slightly boost invoke speed in exchange for text color
+(setq helm-swoop-speed-or-color t)
+
+(helm-mode 1)
+
+;; Package: helm-gtags
+(require 'helm-gtags)
+
+(setq
+ helm-gtags-ignore-case t
+ helm-gtags-auto-update t
+ helm-gtags-use-input-at-cursor t
+ helm-gtags-pulse-at-cursor t
+ helm-gtags-prefix-key "\C-cg"
+ helm-gtags-suggested-key-mapping t
+ )
+
+;; Enable helm-gtags-mode in Dired so you can jump to any tag
+;; when navigate project tree with Dired
+(add-hook 'dired-mode-hook 'helm-gtags-mode)
+
+;; Enable helm-gtags-mode in Eshell for the same reason as above
+(add-hook 'eshell-mode-hook 'helm-gtags-mode)
+
+;; Enable helm-gtags-mode in languages that GNU Global supports
+(add-hook 'c-mode-hook 'helm-gtags-mode)
+(add-hook 'c++-mode-hook 'helm-gtags-mode)
+(add-hook 'java-mode-hook 'helm-gtags-mode)
+(add-hook 'asm-mode-hook 'helm-gtags-mode)
+
+;; key bindings
+(define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
+(define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
+(define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
+(define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
+(define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
+(define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
+
+;; Package: prelude
+(defun prelude-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+Move point to the first non-whitespace character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+If ARG is not nil or 1, move forward ARG - 1 lines first. If
+point reaches the beginning or end of the buffer, stop there."
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+
+(global-set-key (kbd "C-a") 'prelude-move-beginning-of-line)
+
